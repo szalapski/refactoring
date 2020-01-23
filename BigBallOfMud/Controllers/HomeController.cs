eb4749b3fd79c656760fdb468a047339daa3abaa
Patrick Szalapski <patrick.szalapski@genmills.com>
2020-01-22 20:05:12 -0600

    .

--- BigBallOfMud/Controllers/HomeController.cs
+++ BigBallOfMud/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BigBallOfMud.Lib;
 
 namespace BigBallOfMud
@@ -18,6 +14,6 @@ namespace BigBallOfMud
 
         [Route("/")]
         [HttpGet]
-        public string Index() => _provider.GetGreeting();
+        public string Index() => _provider.GetGreeting(false);
     }
 }
--- BigBallOfMud/Lib/GreetingProvider.cs
+++ BigBallOfMud/Lib/GreetingProvider.cs
@@ -1,13 +1,24 @@
 ﻿using System.IO;
+using System.Linq;
 
 namespace BigBallOfMud.Lib
 {
     public class GreetingProvider : IGreetingProvider
     {
-        public string GetGreeting() => $"Hello TCDNUG, this is version {File.ReadAllText("version.txt")}";
+        /* Refactor static/ dependency into proper DI in 6 steps:
+          1. make instance from local
+          2. extract interface
+          3. make tightly-coupled implementation
+          4. bastardize constructor
+          5. configure DI
+          6. remove bastard constructor
+       */
+
+        public string GetGreeting()
+        {
+            string version = File.ReadAllText("version.txt");
+            string parity = int.Parse(version.Split('.').Last()) % 2 == 0 ? "even" : "odd";
+            return $"Hello TCDNUG, this is version {version}.  What an {parity} version!";
+        }
     }
 }
-
-
-// TODO: create test project
-// TODO: fix git HTTP error
