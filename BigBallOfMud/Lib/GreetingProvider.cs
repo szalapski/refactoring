cb15fb31ae07cfaea10ff272cc8f50bc1600eb0a
Patrick Szalapski <Patrick.Szalapski@genmills.com>
2020-02-04 19:33:11 -0600

    .

--- BigBallOfMud/Lib/GreetingProvider.cs
+++ BigBallOfMud/Lib/GreetingProvider.cs
@@ -6,16 +6,19 @@ namespace BigBallOfMud.Lib
     public class GreetingProvider : IGreetingProvider
     {
         /* Refactor static/ dependency into proper DI in 7 steps:
-          1. (static only) Sequester the untestable/uninjectable code into a tightly-coupled implementation
-	        a. extract instance method
-	        b. extract instance class from that method
-	        c. use the new class and method with "new"
-          2. make local refereance for the "new" object only
-          3. move local to instance and switch to abstract type (i.e. interface)
-          4. bastardize constructor
-          5. configure DI
-          7. remove bastard constructor
-          N. clean up code (always)        
+           1. (static only) Sequester the untestable/uninjectable code into a class containing
+            only the tightly-coupled code
+                a. extract instance method
+                b. extract instance class from that method
+                c. use the new class and method with "new"
+            2. make local reference for the "new" object only
+            3. move local to instance 
+            4. modify the instance fields to an abstract type (i.e. interface)
+            5. introduce proper constructor and bastardize old constructor
+            6. configure DI at composition root
+            7. remove bastard constructor
+
+            N. clean up code (always)        
         */
 
         public string GetGreeting()
